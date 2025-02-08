f1ac1278739bd5b019eebe197df79ad378efafec
Ubuntu <ubuntu@ip-10-0-8-99.eu-west-3.compute.internal>
2025-02-08 15:49:43 +0000

    Remove sensitive keys from config

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-OPENWEATHER_API_KEY = "your_openweather_key"
-OPENAI_API_KEY = "your_openai_key"
-SENDER_EMAIL = "your_sender_email"
-RECEIVER_EMAIL = "your_receiver_email"
-SMTP_USER = "your_smtp_user"
-SMTP_PASS = "your_smtp_pass"
-SMTP_SERVER = "email-smtp.eu-west-3.amazonaws.com"
-SMTP_PORT = 587
+import os
+
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
+AWS_SES_USER = os.getenv("AWS_SES_USER")
+AWS_SES_PASS = os.getenv("AWS_SES_PASS")
+SMTP_SERVER = os.getenv("SMTP_SERVER")
